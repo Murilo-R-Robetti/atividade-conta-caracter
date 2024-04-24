69654c38abb77e9dcc5346a1ff1b3e7df8d9c534
Murilo R R <130069044+Murilo-R-Robetti@users.noreply.github.com>
2024-04-24 11:15:26 -0300

    contador de palavras finalizado

--- script.js
+++ script.js
@@ -3,28 +3,29 @@ const divContador = document.querySelector("[txt-contador]")
 const btnContar = document.querySelector("[btn-contar]")
 
 
-let modo = "caracteres"
+let modo = "Caracteres"
 
 
-function handleInputTexto(){
-    if (modo == "caracteres"){
+function handleInputTexto(evento){
+    if (modo == "Caracteres"){
         divContador.innerText = inputTexto.value.length + " Caracteres"
-        // console.log(inputTexto.value.length)
 
-    } else {
-
+    }   else {
+        const palavras = inputTexto.value.trim().split(/\s+/)
+        divContador.innerText = palavras.length + " Palavras"
     }
 }
 
 function handleBtnContar(evento){
-    if (modo === "caracteres"){
+    if (modo === "Caracteres"){
         modo = "palavras"
         btnContar.innerText = "Contar Palavras"
     }else {
-        modo = "caracteres"
+        modo = "Caracteres"
         btnContar.innerText = "Contar Caracteres"
 }
 }
 
 inputTexto.addEventListener("input", handleInputTexto)
 btnContar.addEventListener("click", handleBtnContar)
+btnContar.addEventListener("click", handleInputTexto)
